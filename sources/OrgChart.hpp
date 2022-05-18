730f52d6613e32adfb8c546eef282353386d7f6d
ronsider <ronsider2@gmail.com>
2022-05-18 12:25:29 +0300

    add header file

---
+++ sources/OrgChart.hpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using std::string;
+namespace ariel
+{
+    class OrgChart
+    {
+        public:
+        OrgChart& add_root(const string&);
+        OrgChart& add_sub(const string&, const string&);
+
+        friend ostream& operator <<(ostream& os,OrgChart&);
+    };
+}
